f10738782ef14ce543bc1f992e2e74f304ea57ba
Zach <zachriane01@gmail.com>
2023-02-25 00:13:08 +0800

    [FEAT] Added files for components, pages, and assets

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button() {
+  return (
+    <>
+      <div className="px-6 flex flex-wrap">
+        <button className="mx-2 my-2 bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 rounded border border-gray-800 text-gray-800 px-6 py-2 text-xs">
+          Button
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Button;
