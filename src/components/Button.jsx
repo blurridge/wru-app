c189db8a0f3e27218ccb5ecaa9f8f65859f0dce2
Zach <zachrianemachacon@yahoo.com>
2023-03-01 08:04:49 +0800

    [FEAT] Added ButtonList to Card

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Button = () => {
+export const Button = (name) => {
   return (
     <>
       <div class="flex justify-center space-x-2">
@@ -9,7 +9,7 @@ export const Button = () => {
           data-te-ripple-init
           class="inline-block rounded-full bg-neutral-50 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#fbfbfb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)]"
         >
-          Light
+          Test
         </button>
       </div>
     </>
